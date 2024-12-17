eb54c6b9ca5da1bc26c56c986db1b79f140a2fb0
Пётр <ppe@gelicon.biz>
2024-12-17 16:16:49 +0500

    Восстановил эндпоинт

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ConvectorDocxToPDF
 ConvectorDocxToPDF
+libreService - метод который конвектирует (для удобности)
+appLibreOffice - ендпоинт работающий с методом 
+1) Установка LibreOffice: https://www.libreoffice.org/download/download-libreoffice/
+2) Указать путь до него в libreoffice_path
--- libreService.py
+++ libreService.py
@@ -1,6 +1,6 @@
+# libreService.py
 import os
 import subprocess
-
 
 def convert_docx_to_pdf(docx_path, output_pdf_path):
     """
@@ -11,27 +11,17 @@ def convert_docx_to_pdf(docx_path, output_pdf_path):
     """
     libreoffice_path = r"C:\Program Files\LibreOffice\program\soffice.exe"  # Путь к LibreOffice
 
-    # Выполняем команду LibreOffice
+    # Выполняем команду LibreOffice для конвертации в PDF
     cmd = [
         libreoffice_path, '--headless', '--convert-to', 'pdf',
         '--outdir', os.path.dirname(output_pdf_path), docx_path
-
     ]
 
-    print(f"Запуск команды: {cmd}")
+    print(f"Запуск команды: {cmd}")  # Отладочный вывод для проверки команды
 
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     if result.returncode == 0:
-        print(f"Конвертация завершена. PDF сохранен по пути: {output_pdf_path}")
+        print(f"Конвертация завершена")
     else:
         print(f"Ошибка конвертации: {result.stderr}")
-
-
-# путь к файлу DOCX
-docx_file_path = r"C:\Users\Petr\OneDrive\Рабочий стол\5 семестр\Геликон\Python\KPD\test2.docx"
-# путь для сохранения PDF
-output_pdf_path = r"C:\Users\Petr\OneDrive\Рабочий стол\5 семестр\Геликон\Python\KPD\temp\output.pdf"
-
-# Запускаем конвертацию
-convert_docx_to_pdf(docx_file_path, output_pdf_path)
